7a55bf453df6214a5c6354025119a20e4448fca0
Tanay Chandak <tchandak@tanays-mbp.lan>
2024-03-07 12:39:08 -0600

    changes

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,40 +1,78 @@
-import React from 'react';
+import React from "react";
 
 const HomePage = () => {
   return (
-    <div className="container mx-auto p-4 max-w-5xl"> {/* Adjusted the max-width */}
+    <div className="container mx-auto p-4 max-w-5xl">
+      {" "}
+      {/* Adjusted the max-width */}
       <h1 className="text-4xl font-bold text-center mb-8">CIS 1902: Python</h1>
       <section className="mb-8">
         <h2 className="text-2xl font-semibold mb-2">Staff</h2>
         <ul className="list-disc list-inside">
-            <div><strong>Instructor:</strong><a className="text-indigo-600" href="mailto:tanayc@seas.upenn.edu"> Tanay Chandak</a></div>
-            <div>
-              <strong>Teaching Assistant:</strong><a className="text-indigo-600" href="mailto:jkakadia@seas.upenn.edu"> Jash Kakadia</a>
-              <p className='ml-5'>Office Hours: W 5:30-6:30PM at Levine 5th floor bump space</p>
-            </div>
-            <div>
-              <strong>Teaching Assistant:</strong><a className="text-indigo-600" href="mailto:visheshp@seas.upenn.edu"> Vishesh Patel</a>
-              <p className='ml-5'>Office Hours: S 11-1 PM on Zoom</p>
-            </div>
+          <div>
+            <strong>Instructor:</strong>
+            <a className="text-indigo-600" href="mailto:tanayc@seas.upenn.edu">
+              {" "}
+              Tanay Chandak
+            </a>
+            <p className="ml-5">Office Hours: S 8-9 PM on OHQ</p>
+          </div>
+          <div>
+            <strong>Teaching Assistant:</strong>
+            <a
+              className="text-indigo-600"
+              href="mailto:jkakadia@seas.upenn.edu"
+            >
+              {" "}
+              Jash Kakadia
+            </a>
+            <p className="ml-5">Office Hours: M 7-8 PM on OHQ</p>
+          </div>
+          <div>
+            <strong>Teaching Assistant:</strong>
+            <a
+              className="text-indigo-600"
+              href="mailto:visheshp@seas.upenn.edu"
+            >
+              {" "}
+              Vishesh Patel
+            </a>
+            <p className="ml-5">Office Hours: S 11-1 PM on OHQ</p>
+          </div>
         </ul>
       </section>
       {/* Python Introduction */}
       <section className="mb-8">
         <h2 className="text-2xl font-semibold mb-2">Python: An Overview</h2>
-        <p>Python is a high-level, interpreted programming language known for its simplicity and versatility. It's widely used in various fields, from web development to data science and machine learning. In this course, we explore why Python is a preferred language for developers and data scientists alike.</p>
+        <p>
+          Python is a high-level, interpreted programming language known for its
+          simplicity and versatility. It's widely used in various fields, from
+          web development to data science and machine learning. In this course,
+          we explore why Python is a preferred language for developers and data
+          scientists alike.
+        </p>
       </section>
-
       {/* Syllabus */}
       <section className="mb-8">
         <h2 className="text-2xl font-semibold mb-2">Syllabus Breakdown</h2>
         <ul className="list-disc list-inside">
-          <li><strong>10% Quizzes</strong> - Regular quizzes to assess understanding.</li>
-          <li><strong>10% Attendance</strong> - Active participation in classes.</li>
-          <li><strong>60% Weekly Assignments</strong> - Practical tasks to apply learning.</li>
-          <li><strong>20% Final Project</strong> - A comprehensive project encapsulating course topics.</li>
+          <li>
+            <strong>10% Quizzes</strong> - Regular quizzes to assess
+            understanding.
+          </li>
+          <li>
+            <strong>10% Attendance</strong> - Active participation in classes.
+          </li>
+          <li>
+            <strong>60% Weekly Assignments</strong> - Practical tasks to apply
+            learning.
+          </li>
+          <li>
+            <strong>20% Final Project</strong> - A comprehensive project
+            encapsulating course topics.
+          </li>
         </ul>
       </section>
-
       {/* Course Topics */}
       <section>
         <h2 className="text-2xl font-semibold mb-2">Course Topics</h2>
